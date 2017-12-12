c0997a3008b27f786c5adf8d58e07f1d1368fe03
Tyler Long <tyler4long@gmail.com>
2017-12-12 16:57:54 +0800

    Test password auth

---
+++ test/login.spec.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+import RingCentral from 'ringcentral-ts'
+import secret from '../secret.json'
+
+const rc = new RingCentral({
+  server: secret.server, // You should use SERVER_PRODUCTION in production
+  appKey: secret.appKey,
+  appSecret: secret.appSecret
+})
+
+describe('authorize', () => {
+  test('password authorize', async () => {
+    await rc.auth({
+      username: secret.username,
+      extension: secret.extension,
+      password: secret.password
+    })
+    const token = await rc.getToken()
+    expect(token.accessToken).toBeDefined()
+    expect(token.accessToken.length > 0).toBe(true)
+  })
+})
